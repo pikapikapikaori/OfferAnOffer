f40744b951626bffb485f0774b9380a5fdcaf520
Chunxin Zhang <zcspoi@163.com>
2021-12-31 06:38:10 +0800

    add communication

--- web/src/assets/js/Constant.js
+++ web/src/assets/js/Constant.js
@@ -3,7 +3,7 @@ export default {
     return {
       processBaseUrl: 'http://150.158.95.209:8082',
       loginBaseUrl: 'http://150.158.95.209:8081',
-      positionBaseUrl: 'http://localhost:8072',
+      positionBaseUrl: 'http://150.158.95.209:8072',
     }
   }
 }
