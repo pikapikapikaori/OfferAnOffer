2f2bda52217e8bdc377bb0edd82b748e01a5184d
pikapikapikaori <89848201+pikapikapikaori@users.noreply.github.com>
2021-12-31 07:00:19 +0800

    add web

--- web/src/assets/js/Constant.js
+++ web/src/assets/js/Constant.js
@@ -4,6 +4,7 @@ export default {
       processBaseUrl: 'http://150.158.95.209:8082',
       loginBaseUrl: 'http://150.158.95.209:8081',
       positionBaseUrl: 'http://150.158.95.209:8072',
+      resumeBaseUrl: 'http://150.158.95.209:7777'
     }
   }
 }
