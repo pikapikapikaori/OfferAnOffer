b8c8ce8095404dce65a21170d08522ec0d84d9d3
Chunxin Zhang <zcspoi@163.com>
2021-12-31 00:05:51 +0800

    nav-bar

---
+++ web/src/components/common/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-menu default-active="$route.path" class="el-menu-vertical-demo" mode="horizontal" router="true">
+    <el-menu-item index="/">
+      <i class="el-icon-menu"></i>
+      <span slot="title">主页</span>
+    </el-menu-item>
+    <el-menu-item index="/user/process">
+      <i class="el-icon-menu"></i>
+      <span slot="title">流程</span>
+    </el-menu-item>
+    <el-menu-item index="/auth/login">
+      <i class="el-icon-menu"></i>
+      <span slot="title">登录</span>
+    </el-menu-item>
+    <el-menu-item index="/auth/register">
+      <i class="el-icon-menu"></i>
+      <span slot="title">注册</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
+
